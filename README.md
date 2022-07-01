6647695d03171d81aea48fbc4cbe432d7a54319a
toantt <toantp.91@gmail.com>
2022-07-01 23:29:30 +0700

    add co group by key

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # apache-beams-for-learning
 1. Run command for Joining:
    mvn compile exec:java -Dexec.mainClass=com.techteam.beam.main.JoinExamplesMain -Pdirect-runner
-2. 
--- src/main/java/com/techteam/beam/main/GroupByKeyExamplesMain.java
+++ src/main/java/com/techteam/beam/main/GroupByKeyExamplesMain.java
@@ -1,6 +1,18 @@
 package com.techteam.beam.main;
 
 import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.io.TextIO;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.Filter;
+import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.join.CoGbkResult;
+import org.apache.beam.sdk.transforms.join.CoGroupByKey;
+import org.apache.beam.sdk.transforms.join.KeyedPCollectionTuple;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.TupleTag;
+import org.apache.beam.sdk.values.TypeDescriptors;
 
 public class GroupByKeyExamplesMain {
     public static void main(String[] args) {
@@ -8,10 +20,52 @@ public class GroupByKeyExamplesMain {
     }
 
     private static void runGroupByKeyBigQuery() {
-        Pipeline p = Pipeline.create();
+        String products = "/data/section1/products.csv";
+        String productTypes = "/data/section1/product_types.csv";
+        Pipeline pipeline = Pipeline.create();
+        PCollection<KV<Integer, String>> productCollection =
+                pipeline.apply(TextIO.read().from(products))
+                        .apply("FilterHeader", Filter.by(line ->
+                                !line.isEmpty() && !line.contains("ProductId, ProductName, ProductTypeId, Price")))
+                                .apply(ParDo.of(new DoFn<String, KV<Integer, String>>() {
+                                    @ProcessElement
+                                    public void processElement(ProcessContext c) {
+                                        String row = c.element();
+                                        assert row != null;
+                                        String[] splits = row.split(",");
 
+                                        c.output(KV.of(Integer.valueOf(splits[0]), String.join(",",
+                                                splits[0], splits[1], splits[2], splits[3])));
+                                    }
+                                }));
 
+        PCollection<KV<Integer, String>> productTypeCollection =
+                pipeline.apply(TextIO.read().from(productTypes))
+                        .apply("FilterHeader", Filter.by(line ->
+                                !line.isEmpty() && !line.contains("ProductTypeId, ProductType")))
+                        .apply(ParDo.of(new DoFn<String, KV<Integer, String>>() {
+                            @ProcessElement
+                            public void processElement(ProcessContext c) {
+                                String row = c.element();
+                                assert row != null;
+                                String[] splits = row.split(",");
 
-        p.run().waitUntilFinish();
+                                c.output(KV.of(Integer.valueOf(splits[0]), String.join(",",
+                                        splits[0], splits[1])));
+                            }
+                        }));
+
+        TupleTag<String> productTag = new TupleTag<>();
+        TupleTag<String> productTypeTag = new TupleTag<>();
+
+        PCollection<KV<Integer, CoGbkResult>> result = KeyedPCollectionTuple.of(productTag, productCollection)
+                        .and(productTypeTag, productTypeCollection).apply(CoGroupByKey.create());
+
+        result.apply("Preview grouped data",
+                MapElements.into(TypeDescriptors.strings()).via(
+                        x -> { System.out.println(x); return ""; })
+        );
+
+        pipeline.run().waitUntilFinish();
     }
 }
