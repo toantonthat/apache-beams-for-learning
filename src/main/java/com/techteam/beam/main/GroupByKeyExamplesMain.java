c48c5c97a0dd04731baa2b4a82c347ebf14d8bdf
toantt <toantp.91@gmail.com>
2022-07-01 16:12:53 +0700

    update read me

--- src/main/java/com/techteam/beam/main/GroupByKeyExamplesMain.java
+++ src/main/java/com/techteam/beam/main/GroupByKeyExamplesMain.java
@@ -1,4 +1,17 @@
 package com.techteam.beam.main;
 
+import org.apache.beam.sdk.Pipeline;
+
 public class GroupByKeyExamplesMain {
+    public static void main(String[] args) {
+        runGroupByKeyBigQuery();
+    }
+
+    private static void runGroupByKeyBigQuery() {
+        Pipeline p = Pipeline.create();
+
+
+
+        p.run().waitUntilFinish();
+    }
 }
